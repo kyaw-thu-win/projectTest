4a0e6fe213f22482411a19e6e50e3a408f28d695
Kyaw Kyaw <ktw2305873@gmail.com>
2023-06-19 23:49:19 +0630

    deploy test 5

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git checkout -B main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
+# if your are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io<REPO>
