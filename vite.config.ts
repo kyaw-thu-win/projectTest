5189874204dfe944b8fd900862b5c1691ebcf3eb
Kyaw Kyaw <ktw2305873@gmail.com>
2023-06-19 23:12:54 +0630

    deploy test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/projectTest/",
+  base: "/projectTest",
   build: { chunkSizeWarningLimit: 1600 },
 });
